6ad009dd617112fedfd1d1c92792b0f995c0c7f2
ausf-software <77770487+ausf-software@users.noreply.github.com>
2023-04-01 21:53:10 +0300

    Backup

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
-
-var ob = new StateMachine("1*n+2*m-3");
-ob.parseFunction();
-ob.setsInit();
-ob.calculateState();
-console.log(ob.toString());
+window.onload = function () {
+	document.getElementById("menu").onclick = function(){
+	var x = document.getElementById('site_menu');
+		if (x.className === "site_menu") {
+		x.className += " responsive";
+		} else {
+		x.className = "site_menu";
+		}
+	}
+	document.getElementById("function-submit").onclick = function(){
+		var input = document.getElementById('function-input');
+		var ob = new StateMachine(input.value);
+		ob.parseFunction();
+		ob.setsInit();
+		ob.calculateState();
+		console.log(ob.toString());	
+	}
+};
+//1*n+2*m-3
